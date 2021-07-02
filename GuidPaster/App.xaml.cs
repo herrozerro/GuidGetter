f4f5589fb9d550ae09a8c627f77490c3ed64bbed
Josiah Bradbury <herrozerro@gmail.com>
2021-07-02 00:04:59 -0500

    got it working

--- GuidPaster/App.xaml.cs
+++ GuidPaster/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 using Indieteur.GlobalHooks;
 
 namespace GuidPaster
@@ -15,27 +16,32 @@ namespace GuidPaster
     public partial class App : Application
     {
         public static GlobalKeyHook GlobalKeyHook = null;
+        public static GuidGenerator wndGuidGenerator = null;
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             GlobalKeyHook = new GlobalKeyHook();
             GlobalKeyHook.OnKeyDown += OpenGuidGenerator;
 
+            //TaskbarIcon tbi = new TaskbarIcon();
+            //tbi.Icon = new System.Drawing.Icon("./Icons/Hammer.ico");
+            //tbi.ToolTipText = "hello world";
 
-            // Create the startup window
-            MainWindow wnd = new MainWindow();
-            // Do stuff here, e.g. to the window
-            wnd.Title = "Something else";
-            // Show the window
-            wnd.Show();
+            Taskbar window = new Taskbar();
+            window.ShowInTaskbar = false;
+            window.Show();
         }
 
         private static void OpenGuidGenerator(object sender, GlobalKeyEventArgs e)
         {
             if (e.Control != ModifierKeySide.None && e.Alt != ModifierKeySide.None && e.KeyCode == VirtualKeycodes.Q)
             {
-                GuidGenerator guidGenerator = new GuidGenerator();
-                guidGenerator.Show();
+                if (wndGuidGenerator == null)
+                {
+                    wndGuidGenerator = new GuidGenerator();
+                    wndGuidGenerator.Closing += (a, b) => wndGuidGenerator = null;
+                }
+                wndGuidGenerator.Show();
             }
         }
     }
---
+++ GuidPaster/Taskbar.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuidPaster
+{
+    /// <summary>
+    /// Interaction logic for Taskbar.xaml
+    /// </summary>
+    public partial class Taskbar : Window
+    {
+        public Taskbar()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+
+    public class ShowCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            if (App.wndGuidGenerator == null)
+            {
+                App.wndGuidGenerator = new GuidGenerator();
+                App.wndGuidGenerator.Closing += (a, b) => App.wndGuidGenerator = null;
+            }
+            App.wndGuidGenerator.Show();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
