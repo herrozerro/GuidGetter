10ab96c9de78d4db2aed029c01736b6bb14fa0d9
Josiah Bradbury <herrozerro@gmail.com>
2023-02-08 21:24:57 -0600

    updated to dotnet6

--- GuidGetter/Taskbar.xaml.cs
+++ GuidGetter/Taskbar.xaml.cs
@@ -42,11 +42,11 @@ namespace GuidGetter
             App.wndGuidGenerator.Show();
         }
 
+        public event EventHandler? CanExecuteChanged;
+
         public bool CanExecute(object parameter)
         {
             return true;
         }
-
-        public event EventHandler CanExecuteChanged;
     }
 }
